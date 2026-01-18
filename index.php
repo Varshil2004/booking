c09086f4aa8b588f6bd3a4c9c6b9ecdd7b873c26
KUKADIYA VARSHIL <152945612+Varshil2004@users.noreply.github.com>
2026-01-18 13:49:23 +0530

    Add comment for booking website functionality
    Added a comment explaining the purpose of the PHP code.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// this is a php code for booking website which help to book hotels according to your choice.
 include 'components/connect.php';
 if(isset($_COOKIE['user_id'])){
    $user_id = $_COOKIE['user_id'];
