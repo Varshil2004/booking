86029fc0a643e87b0206fe939c421f6ad43cae6a
KUKADIYA VARSHIL <152945612+Varshil2004@users.noreply.github.com>
2026-08-24 07:43:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-// index page code for booking website.
+// index page code for booking website.which help to booking process.
 <?php
 include 'components/connect.php';
 if(isset($_COOKIE['user_id'])){
